8e32bce5f0a8a9a52277eae9972e15d3a4f20e39
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-11-13 22:18:55 +0300

    fetch mainPage

--- diplom_solution/src/components/Catalog.js
+++ diplom_solution/src/components/Catalog.js
@@ -3,9 +3,55 @@ import PropTypes from 'prop-types'
 
 function Catalog(props) {
     return (
-        <div>
-            
+        <main className="container">
+        <div className="row">
+            <div className="col">
+                
+
+                <section className="catalog">
+                    <h2 className="text-center">Каталог</h2>
+
+                    <form className="catalog-search-form form-inline">
+                        <input className="form-control" placeholder="Поиск" />
+                    </form>
+
+                    <ul className="catalog-categories nav justify-content-center">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Все</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Женская обувь</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Мужская обувь</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Обувь унисекс</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Детская обувь</a>
+                        </li>
+                    </ul>
+
+                    {/* <div className="col-4">
+                        <div className="card catalog-item-card">
+                            <img src="https://cdn-images.farfetch-contents.com/12/99/04/32/12990432_13705715_1000.jpg"
+                                className="card-img-top img-fluid" alt="Супергеройские кеды" />
+                            <div className="card-body">
+                                <p className="card-text">Супергеройские кеды</p>
+                                <p className="card-text">1 400 руб.</p>
+                                <a href="/products/1.html" className="btn btn-outline-primary">Заказать</a>
+                            </div>
+                        </div>
+                    </div> */}
+                    <div className="text-center">
+                        <button className="btn btn-outline-primary">Загрузить ещё</button>
+                    </div>
+                </section>
+
+            </div>
         </div>
+    </main>
     )
 }
 
--- diplom_solution/src/components/MainPage.js
+++ diplom_solution/src/components/MainPage.js
@@ -1,9 +1,50 @@
-import React from 'react'
-import banner from '../img/banner.jpg'
+import React, {useEffect, useState} from 'react'
+import {nanoid} from 'nanoid'
 import PropTypes from 'prop-types'
 import Banner from './Banner'
 
 function MainPage(props) {
+    const [state, setstate] = useState({
+        categories: [],
+        itemsHit: [],
+        itemsAll: [],
+        loadding: true,
+        loaddingCategories: true,
+        loaddingCatalogAll: true
+    });
+
+    useEffect(() => {
+        const fetchHadlerHit = async () => {
+            const response = await fetch('http://localhost:7070/api/top-sales');
+            const hit = await response.json();
+            console.log(hit);
+            setstate(prevstate => ({...prevstate, itemsHit: hit, loadding: false}));
+        }
+        fetchHadlerHit();
+    }, [])
+
+    useEffect(() => {
+        const fetchHadlerCategories = async () => {
+            const response = await fetch('http://localhost:7070/api/categories');
+            const categories = await response.json();
+            console.log(categories);
+            setstate(prevstate => ({...prevstate, categories: categories, loaddingCategories: false}));
+        }
+        fetchHadlerCategories();
+    }, [])
+
+    useEffect(() => {
+        const fetchHadlerCatalogAll = async () => {
+            const response = await fetch('http://localhost:7070/api/items');
+            const allItems = await response.json();
+            console.log(allItems);
+            setstate(prevstate => ({...prevstate, itemsAll: allItems, loaddingCatalog: false}));
+        }
+        fetchHadlerCatalogAll();
+    }, [])
+
+    console.log(state);
+
     return (
         <div className="container">
             <div className="row">
@@ -12,24 +53,79 @@ function MainPage(props) {
 
                     <section className="top-sales">
                         <h2 className="text-center">Хиты продаж!</h2>
-
-                        <div className="preloader">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
+                        
+                        <div className="row">
+                        {state.itemsHit.sort((a, b) => {return a.price - b.price}).map(el =>                         
+                            <div key={nanoid()} className="col-4">
+                                <div className="card">
+                                    <img src={el.images[0]}
+                                        className="card-img-top img-fluid" alt="Босоножки 'MYER'" />
+                                    <div className="card-body">
+                                        <p className="card-text">{el.title}</p>
+                                        <p className="card-text">{el.price} руб.</p>
+                                        <a href="/products/1.html" className="btn btn-outline-primary">Заказать</a>
+                                    </div>
+                                </div>
+                            </div>    
+                        )}
                         </div>
+
+                        {state.loadding ? 
+                            <div className="preloader">
+                               <span></span>
+                               <span></span>
+                               <span></span>
+                               <span></span>
+                            </div> : null}
                     </section>
 
                     <section className="catalog">
                         <h2 className="text-center">Каталог</h2>
 
-                        <div className="preloader">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
+                        {state.categories.length > 0 ? 
+                            <ul className="catalog-categories nav justify-content-center">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="#">Все</a>
+                                </li>
+                                {state.categories.map(el => 
+                                    <li key={el.id} className="nav-item">
+                                        <a className="nav-link" href="#">{el.title}</a>
+                                    </li>
+                                )}
+                            </ul>
+
+                            
+                        : null}
+
+                        
+                        
+                        <div className="row">
+                            {state.itemsAll.map(el => 
+                                <div class="col-4">
+                                <div class="card catalog-item-card">
+                                    <img src={el.images[1]}
+                                        class="card-img-top img-fluid" alt="Босоножки 'MYER'" />
+                                    <div class="card-body">
+                                        <p class="card-text">{el.title}</p>
+                                        <p class="card-text">{el.price}</p>
+                                        <a href="/products/1.html" class="btn btn-outline-primary">Заказать</a>
+                                    </div>
+                                </div>
+                                </div>    
+                            )}
                         </div>
+
+                        <div class="text-center">
+                            <button class="btn btn-outline-primary">Загрузить ещё</button>
+                        </div>
+
+                        {state.loaddingCatalog ? 
+                            <div className="preloader">
+                               <span></span>
+                               <span></span>
+                               <span></span>
+                               <span></span>
+                            </div> : null}
                     </section>
 
                 </div>
